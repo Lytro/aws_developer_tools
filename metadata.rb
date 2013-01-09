50bc18fc5479f2e887fa87bb0fb0827e5a917725
Anuj Biyani <abiyani@lytro.com>
2013-01-09 14:32:01 -0800

    bump version to 0.1.1 add some more information to the metadata

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,12 @@ maintainer_email "anujbiyani01@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures chef_ec2_cli_tools"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 depends 'java', '~> 1.8.0'
+
+recipe "chef_ec2_cli_tools", "setup both Amazon AMI and API tools."
+recipe "chef_ec2_cli_tools::ami", "only Amazon AMI tools"
+recipe "chef_ec2_cli_tools::api", "only Amazon API tools."
+
+replaces "ec2-ami-tools"
