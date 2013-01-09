a0e17a8e31162db9c3b8e2c9f2fa0acb63fac0b4
Anuj Biyani <abiyani@lytro.com>
2013-01-08 18:21:42 -0800

    download and unzip ec2-api-tools.zip

--- recipes/api.rb
+++ recipes/api.rb
@@ -4,6 +4,17 @@
 #
 package "unzip"
 
+remote_file "/tmp/ec2-api-tools.zip" do
+  source "http://s3.amazonaws.com/ec2-downloads/ec2-api-tools.zip"
+end
+
+bash "unzip ec2-api-tools" do
+  cwd "/tmp"
+  code <<-EOH
+    unzip -od #{node["chef_ec2_cli_tools"]["install_root"]} ./ec2-api-tools.zip
+  EOH
+end
+
 template "/etc/profile.d/ec2_tools.sh" do
   source "ec2_tools.sh.erb"
   owner "root"
--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -4,4 +4,8 @@ describe 'chef_ec2_cli_tools::api' do
   let(:chef_run) { ChefSpec::ChefRunner.new.converge 'chef_ec2_cli_tools::api' }
 
   it_behaves_like "ec2 cli tools"
+
+  it "downloads ec2-api-tools.zip" do
+    chef_run.should create_remote_file "/tmp/ec2-api-tools.zip"
+  end
 end
