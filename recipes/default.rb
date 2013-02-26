fdb8a70bf0c72152aada6ad0722ad7974d81b816
Anuj Biyani <abiyani@lytro.com>
2013-02-26 15:27:04 -0800

    include as recipe in default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,5 +3,6 @@
 # Recipe:: default
 #
 
-include_recipe "chef_ec2_cli_tools::ami"
-include_recipe "chef_ec2_cli_tools::api"
+include_recipe 'chef_ec2_cli_tools::ami'
+include_recipe 'chef_ec2_cli_tools::api'
+include_recipe 'chef_ec2_cli_tools::as'
