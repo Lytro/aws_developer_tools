1465309fc86152a54da220f0d95cc75be920bc0e
Anuj Biyani <abiyani@lytro.com>
2013-02-26 15:36:52 -0800

    set the default mode to 0755 for aws_credentials and aws_as.sh

--- recipes/as.rb
+++ recipes/as.rb
@@ -6,5 +6,10 @@ include_recipe 'java' do
   only_if { node['chef_ec2_cli_tools']['install_java?'] }
 end
 
-cookbook_file "#{node['chef_ec2_cli_tools']['install_target']}/aws_credentials"
-cookbook_file '/etc/profile.d/aws_as.sh'
+cookbook_file "#{node['chef_ec2_cli_tools']['install_target']}/aws_credentials" do
+  mode 0755
+end
+
+cookbook_file '/etc/profile.d/aws_as.sh' do
+  mode 0755
+end
