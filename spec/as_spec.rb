192523c58762cbd1b1db0fed629c256d9fdb65c6
Anuj Biyani <abiyani@lytro.com>
2013-02-26 16:57:17 -0800

    fix aws_as.sh

--- spec/as_spec.rb
+++ spec/as_spec.rb
@@ -7,6 +7,6 @@ describe 'chef_ec2_cli_tools::as' do
 
   it 'sets up the aws credentials and auto scaling home dir' do
     expect(chef_run).to create_file_with_content '/usr/local/share/ec2_tools/aws_credentials', %Q{AWSAccessKeyId=$AWS_ACCESS_KEY\nAWSSecretKey=$AWS_SECRET_KEY\n}
-    expect(chef_run).to create_file_with_content '/etc/profile.d/aws_as.sh', %Q{AWS_CREDENTIAL_FILE=$EC2_HOME/aws_credentials\nAWS_AUTO_SCALING_HOME=$EC2_HOME\n}
+    expect(chef_run).to create_file_with_content '/etc/profile.d/aws_as.sh', %Q{export AWS_CREDENTIAL_FILE=$EC2_HOME/aws_credentials\nexport AWS_AUTO_SCALING_HOME=$EC2_HOME\n}
   end
 end
