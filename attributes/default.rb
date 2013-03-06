e91cb0b06551f9fb8cff88200075e29769b03191
Anuj Biyani <abiyani@lytro.com>
2013-03-05 16:17:39 -0800

    fix references to other attributes from with attributes/default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,10 +8,10 @@ default['aws_developer_tools']['aws_secret_key'] = 'Your Secret Key'
 default['aws_developer_tools']['ec2_tools_target'] = '/usr/local/share/ec2_tools'
 
 default['aws_developer_tools']['api']['source'] = 'http://s3.amazonaws.com/ec2-downloads/ec2-api-tools.zip'
-default['aws_developer_tools']['api']['install_target'] = node['aws_developer_tools']['install_target']
+default['aws_developer_tools']['api']['install_target'] = default['aws_developer_tools']['install_target']
 
 default['aws_developer_tools']['ami']['source'] = 'http://s3.amazonaws.com/ec2-downloads/ec2-ami-tools.zip'
-default['aws_developer_tools']['ami']['install_target'] = node['aws_developer_tools']['install_target']
+default['aws_developer_tools']['ami']['install_target'] = default['aws_developer_tools']['install_target']
 ###############
 
 ## AWS Tools ##
