a247073d613f35a4e8288b8c2df10c0881ca4b1d
Anuj Biyani <abiyani@lytro.com>
2013-03-05 16:29:21 -0800

    refactor more specs to avoid using attributes; fix some attributes

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -6,15 +6,16 @@ describe 'aws_developer_tools::api' do
   it_behaves_like 'cli tools', 'api'
   it_behaves_like 'ec2 tools'
 
-  it "installs java" do
+  it 'installs java' do
     expect(chef_run).to include_recipe 'java'
   end
 
-  it "exports the AWS_ACCESS_KEY and AWS_SECRET_KEY" do
+  it 'exports the AWS_ACCESS_KEY and AWS_SECRET_KEY' do
     expect(chef_run).to create_file_with_content '/etc/profile.d/aws_keys.sh',
-                                             "export AWS_ACCESS_KEY=#{@runner.node['aws_developer_tools']['aws_access_key']}"
+                                                 'export AWS_ACCESS_KEY=Your Access Key'
     expect(chef_run).to create_file_with_content '/etc/profile.d/aws_keys.sh',
-                                             "export AWS_SECRET_KEY=#{@runner.node['aws_developer_tools']['aws_secret_key']}"
+                                                 'export AWS_SECRET_KEY=Your Secret Key'
+
     expect(chef_run.template('/etc/profile.d/aws_keys.sh')).to be_owned_by('root', 'root')
   end
 end
