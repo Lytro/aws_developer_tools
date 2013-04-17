c93e294a43338792ceb443aff85c5413e7ae2a7b
Anuj Biyani <abiyani@lytro.com>
2013-04-17 15:09:46 -0700

    bump to version 3.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@lytro.com'
 license          'Apache 2.0'
 description      'Installs/Configures aws_developer_tools'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '3.0.0'
+version          '3.1.0'
 
 depends 'java', '>= 1.8.0'
 depends 'chef_ruby', '>= 2.2.0'
