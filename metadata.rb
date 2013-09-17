87e779c9b5e1b126a8a1bec69b1e0fd5046d7dd2
Chris Fordham <chris@xhost.com.au>
2013-09-17 16:23:46 +1000

    Add in some attributes to metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -18,3 +18,35 @@ recipe "aws_developer_tools::elb", "Installs Elastic Load Balancing tools (requi
 recipe "aws_developer_tools::aws_ha_release",  "Installs AWS HA Release. Third-party script that replaces old instances with new ones; used when deploying new code to ensure all active instances are using the fresh code. Since this is a third-party script, it is not a part of the default recipe."
 
 replaces 'chef_ec2_cli_tools'
+
+attribute "aws_developer_tools/install_java?",
+  :display_name => "AWS Developer Tools install java",
+  :description => "Whether to automatically install Java with Amazon API Tools.",
+  :required => "optional",
+  :recipes => [ "aws_developer_tools:api" ],
+  :choice => [ "true", "false" ],
+  :default => "true"
+
+attribute "aws_developer_tools/install_ruby?",
+  :display_name => "AWS Developer Tools install ruby",
+  :description => "Whether to automatically install Java with Amazon AMI Tools.",
+  :required => "optional",
+  :recipes => [ "aws_developer_tools:ami" ],
+  :choice => [ "true", "false" ],
+  :default => "true"
+
+attribute "aws_developer_tools/aws_access_key",
+  :display_name => "AWS Developer Tools access key",
+  :description => "The AWS Access Key for use with AWS developer tools.",
+  :required => "required"
+  
+attribute "aws_developer_tools/aws_secret_key",
+  :display_name => "AWS Developer Tools secrete key",
+  :description => "The AWS Secret Key for use with AWS developer tools.",
+  :required => "required"
+
+attribute "aws_developer_tools/ec2_tools_target",
+  :display_name => "AWS Developer Tools EC2 tools target",
+  :description => "The directory to install EC2 tools into.",
+  :required => "optional",
+  :default => "/usr/local/share/ec2_tools"
