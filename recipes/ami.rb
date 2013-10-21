9da189a32efeaa1ba66f23f2bb8eb122285e32b2
Chris Fordham <chris@xhost.com.au>
2013-10-21 15:05:02 +1100

    Do not use resource conditional options as they are not supported with include_recipe.

--- recipes/ami.rb
+++ recipes/ami.rb
@@ -1,6 +1,4 @@
-include_recipe 'chef_ruby' do
-  only_if { node['aws_developer_tools']['install_ruby?'] && node['aws_developer_tools']['install_ruby?'] != 'false' }
-end
+include_recipe 'chef_ruby' unless (!node['aws_developer_tools']['install_ruby?'] || node['aws_developer_tools']['install_ruby?'] == 'false')
 
 cli_tools 'ami' do
   source node['aws_developer_tools']['ami']['source']
