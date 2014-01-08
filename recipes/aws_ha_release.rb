d6550adcfb7f21964eae7eca7f10ecf44cb51185
Anuj Biyani <abiyani@lytro.com>
2014-01-07 16:35:35 -0800

    update aws-ha-release.sh url

--- recipes/aws_ha_release.rb
+++ recipes/aws_ha_release.rb
@@ -2,6 +2,6 @@ include_recipe 'aws_developer_tools::auto_scaling'
 include_recipe 'aws_developer_tools::elb'
 
 remote_file "#{node['aws_developer_tools']['auto_scaling']['install_target']}/bin/aws-ha-release.sh" do
-  source 'https://raw.github.com/colinbjohnson/aws-missing-tools/master/bin/aws-ha-release.sh'
+  source 'https://raw.github.com/colinbjohnson/aws-missing-tools/master/aws-ha-release/aws-ha-release.sh'
   mode 0755
 end
