5e871bcff65e65e8c2f76830d07af5e1a1f75432
Anuj Biyani <abiyani@lytro.com>
2014-07-29 11:25:59 -0700

    change license to Do What the Fuck You Want to Public License (http://www.wtfpl.net/)

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'aws_developer_tools'
 maintainer       'Lytro'
 maintainer_email 'cookbooks@lytro.com'
-license          'Apache 2.0'
+license          'WTFPL'
 description      'Installs/Configures aws_developer_tools'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '3.2.0'
